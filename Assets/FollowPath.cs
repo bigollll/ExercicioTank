363e45aa42e34d6e986021fb1a52847cbc2b7987
bigollll <rodrigobego@uol.com.br>
2021-04-10 19:49:35 -0300

    update
    tarefa tank

---
+++ Assets/FollowPath.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    Transform goal;
+    public float speed = 5.0f;    
+    float accuracy = 1.0f;
+    float rotSpeed = 2.0f;
+    
+    public GameObject wpManager;
+    GameObject[] wps;
+    GameObject currentNode;
+    
+    int currentWP = 0;
+    Graph g;
+    void Start()
+    {
+        //setando variaveis 
+        wps = wpManager.GetComponent<WPManager>().waypoints;
+        g = wpManager.GetComponent<WPManager>().graph;
+        
+        //nó que começa
+        currentNode = wps[0];
+    }
+
+    // 3 metodos que dão direcionamento
+    public void GoToHeli()
+    {
+        //pega o grafh
+        g.AStar(currentNode, wps[6]);
+        currentWP = 0;
+        //define como 0 qnd termina tudo
+    }
+
+    public void GoToRuin()
+    {
+        g.AStar(currentNode, wps[3]);
+        currentWP = 0;
+    }
+
+    public void GoToFabrica()
+    {
+       
+        g.AStar(currentNode, wps[10]);
+        currentWP = 0;
+    }
+
+    void LateUpdate()
+    {
+        //zera qnd finaliza
+        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
+          return;
+
+        currentNode = g.getPathPoint(currentWP);
+        //nó = wp
+
+        
+        //soma wp atual dependendo da distancia entre pontos
+        if (Vector3.Distance(g.getPathPoint(currentWP).transform.position,transform.position) < accuracy)
+        {
+            currentWP++;
+        }
+
+        if (currentWP < g.getPathLength())
+        {
+            //proximo wp
+            goal = g.getPathPoint(currentWP).transform;
+            //ve o proximo wp
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y,goal.position.z);
+            //rotaciona 
+            Vector3 direction = lookAtGoal - this.transform.position; this.transform.rotation = Quaternion.Slerp(this.transform.rotation,Quaternion.LookRotation(direction),Time.deltaTime * rotSpeed);
+        }
+        //linha que faltava: movimentação 
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/WPManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public struct Link
+{
+    public enum direction { UNI, BI }
+    public GameObject node1;
+    public GameObject node2;
+    public direction dir;
+}
+
+public class WPManager : MonoBehaviour
+{
+    //Wps no mapa
+    public GameObject[] waypoints;
+
+    //ligação dos wps
+    public Link[] links;
+
+    //pega o graph
+    public Graph graph = new Graph();
+
+
+    void Start()
+    {
+        // ve se o array n ta vazio
+        if (waypoints.Length > 0)
+        {
+            //quantidade de objetos 
+            foreach (GameObject wp in waypoints)
+            {
+                graph.AddNode(wp);
+                //adiciona node pro obj
+            }
+
+            //qntidade de obj no array
+            foreach (Link l in links)
+            {
+                //graph pra adc um Edge
+                graph.AddEdge(l.node1, l.node2);
+                //vê se é bi ou não
+                if (l.dir == Link.direction.BI)  
+                    graph.AddEdge(l.node2, l.node1);
+                //graph pra adc um Edge
+            }
+        }
+    }
+    void Update()
+    {
+        graph.debugDraw();
+        //mostra as linhas
+    }
+}
+
